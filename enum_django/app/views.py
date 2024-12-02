213378e1c175e0ea59dcd29b8891ba29a8181b7b
Soumya Nasipuri <nasipurisoumya@gmail.com>
2024-12-03 03:39:15 +0530

    :lipstick: UI Change: Homepage structure is ready

--- enum_django/app/views.py
+++ enum_django/app/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.views import View
-from django.http import HttpResponse
+# from django.http import HttpResponse
+# from django.template import loader
 
 # Create your views here.
 class Index(View):
-    template_name = ""
+    template_name = "app/index.html"
     
     def get(self, request, *args, **kwargs):
         context = {}
-        # render(request = request, template_name = self.template_name, context = context)
-        return HttpResponse("Hello, world. You're at the polls index.")
+        return render(request = request, template_name = self.template_name, context = context)
+        # return HttpResponse("Hello, world. You're at the polls index.")
     
     def post(self, request, *args, **kwargs):
         pass
